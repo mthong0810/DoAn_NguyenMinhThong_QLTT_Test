6a7c685f0de05c5c58f12918a6498b9ac9a37e34
mthong0810 <lonelynut2k@gmail.com>
2020-04-19 20:55:45 +0700

    Đây là bài test project

---
+++ src/doan_nguyenminhthong_qltt_test/DoAn_NguyenMinhThong_QLTT_Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doan_nguyenminhthong_qltt_test;
+
+/**
+ *
+ * @author ACER
+ */
+public class DoAn_NguyenMinhThong_QLTT_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
